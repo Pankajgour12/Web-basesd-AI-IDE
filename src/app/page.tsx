f493f71c9e38daa18e29656367dcc140e1c001b7
pankajgour12 <pankajgour5000@gmail.com>
2026-01-28 19:22:08 +0530

    prisma connect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-500 font-sans ">
        <h1 className=" text-4xl text-rose-600"> this is home page </h1>
+
+       this is home page page 
        
         <Button> click me </Button>
       
